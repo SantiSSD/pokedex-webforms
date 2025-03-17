392862da086f06ff5a9868af4f7a8a30b0c89985
Santiago <santiago11gimenez@gmail.com>
2025-03-16 21:45:30 -0300

    se añadio el dominio y negocio

---
+++ negocio/ElementosNegocio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using winform_app;
+
+namespace negocio
+{
+    public class ElementosNegocio
+    {
+        public List<Elemento> listar()
+        {
+            List<Elemento> lista = new List<Elemento>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Select Id, Descripcion From ELEMENTOS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Elemento aux = new Elemento();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+    }
+
+
+
+
+
+            
+        
+   
+
